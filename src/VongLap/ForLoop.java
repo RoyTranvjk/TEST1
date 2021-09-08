0450c7d9ff57cc1782b3d157ff3f347312850de0
RoyTranvjk <you@example.com>
2021-09-08 17:40:22 +0700

    FIRST COMMIT

---
+++ src/VongLap/ForLoop.java
@@ -0,0 +1,14 @@
+package VongLap;
+
+public class ForLoop {
+
+    // 001 LOOP
+    public static void main(String[] args) {
+       int number =8;
+        //FOR
+        for(int i=1; i<10; i++) { // i bắt đầu bằng bao nhiêu
+            // System.out.println(number);
+            System.out.println("Java is cool");// nếu không có vòng lặp thì muốn in 10 kết quả phải viết 10 lần, nên dùng vòng for
+        }
+    }
+}
